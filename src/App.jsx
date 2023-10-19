05d4f5a5c8563771ce6b18d14a66c197f487755e
Emmanuel-222 <emmanuelogbuefi43@gmail.com>
2023-10-19 18:00:37 +0100

    A little update on the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,29 @@ function App() {
 
 
   return (
-    <div className='flex m-5 border-blue-700 bg-gray-500'>
+    <div className='flex-col m-5 border-blue-700 w-full h-full pt-10 px-40 pb-40 bg-blue-950 text-center items-center shadow-2xl shadow-black'>
       <div>
+        <h1 className='text-3xl mb-5'>Weather App</h1>
         <input 
+        className='p-1.5 w-9/12 mr-2 outline-none rounded-lg'
         type="text" 
         />
-        <button>Search</button>
+        <button
+        className='bg-blue-700 rounded-lg p-2 text-white'
+        >
+          Search
+        </button>
+      </div>
+
+      <div className='flex-col items-center justify-evenly mt-10 p-5 w-full h-full bg-gray-500 shadow-2xl shadow-black opacity-50 text-white'>
+        <h1>HEllo</h1>
+        <h1>HEllo</h1>
+        <h1>HEllo</h1>
+        <h1>HEllo</h1>
+        <h1>HEllo</h1>
+        <h1>HEllo</h1>
+        <h1>HEllo</h1>
+        <h1>HEllo</h1>
       </div>
     </div>
   )
