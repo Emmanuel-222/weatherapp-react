22a3c14567a534187128f8d2d5becab980a1f551
Emmanuel-222 <emmanuelogbuefi43@gmail.com>
2023-10-19 16:42:48 +0100

    first-commit for my weather app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='flex m-5 border-blue-700 bg-gray-500'>
+      <div>
+        <input 
+        type="text" 
+        />
+        <button>Search</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
